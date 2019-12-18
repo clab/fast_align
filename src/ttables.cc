4e4c64b9473b21e85be89be924fb97ea921d93cd
Chris Dyer <cdyer@google.com>
2019-12-18 14:19:36 +0000

    fix warning

--- src/ttables.cc
+++ src/ttables.cc
@@ -1,25 +1,23 @@
 #include "src/ttables.h"
 
+#include <cmath>
 #include <string>
 #include <fstream>
 
 #include "src/corpus.h"
 
-using namespace std;
-
-void TTable::DeserializeLogProbsFromText(istream* in, Dict& d) {
+void TTable::DeserializeLogProbsFromText(std::istream* in, Dict& d) {
   int c = 0;
-  string e;
-  string f;
+  std::string e, f;
   double p;
   while(*in) {
     (*in) >> e >> f >> p;
     if (e.empty()) break;
     ++c;
     unsigned ie = d.Convert(e);
-    if (ie >= static_cast<int>(ttable.size())) ttable.resize(ie + 1);
-    ttable[ie][d.Convert(f)] = exp(p);
+    if (ie >= ttable.size()) ttable.resize(ie + 1);
+    ttable[ie][d.Convert(f)] = std::exp(p);
   }
-  cerr << "Loaded " << c << " translation parameters.\n";
+  std::cerr << "Loaded " << c << " translation parameters.\n";
 }
 
